0afe47d0435e567417c59e5d620665c1aa5dcd45
Furqan Baig <furqan@Furqans-MacBook-Pro.local>
2016-09-28 15:37:26 -0400

    Grid changed to 2D-list instead of dictionary

--- a1/puzzleSolver.py
+++ a1/puzzleSolver.py
@@ -11,42 +11,37 @@ class State(object):
     ''' Singleton State class to track each tile in the puzzle grid '''
     
     __instance = None
-    def __new__(cls, version):
+    def __new__(cls, version, initFileName):
         if State.__instance is None:
             State.__instance = object.__new__(cls)
             State.__instance.version = version
             # initilize grid
-            State.__instance.Grid = {}
+            # read in initial values from input file
+            import csv
+            with open(initFileName, 'r') as f:
+                reader = csv.reader(f)
+                State.__instance.Grid = list(reader)
         return State.__instance
 
 
-    def __init__(self, initFileName):
-        # read in initial values from input file
-        # CHANGE: hardcoded for now
-
-        self.setTileValue(0,0, 1)
-        self.setTileValue(0,1, 3)
-        self.setTileValue(0,2, BLANK)
-        self.setTileValue(1,0, 4)
-        self.setTileValue(1,1, 2)
-        self.setTileValue(1,2, 5)
-        self.setTileValue(2,0, 7)
-        self.setTileValue(2,1, 8)
-        self.setTileValue(2,2, 6)
+    def __init__(self, version, initFileName):
+        #self.printGrid()
+        print ('Constructor ...')
 
     def setTileValue(self, i, j, val):
+        # check for out of bound indices
         if i < 0 or i > self.__instance.version or j < 0 or j > self.__instance.version:
             print ('ERROR')
-        self.__instance.Grid[i, j] = val
+        self.__instance.Grid[i][j] = val
 
     def getTileValue(self, i, j):
-        return self.__instance.Grid[i, j]
+        return self.__instance.Grid[i][j]
 
     def printGrid(self):
-        print (self.__instance.Grid)
-        #for i in xrange(0, self.__instance.version):
-        #    for j in xrange(0, self.__instance.version):
-                
+        for i in range(self.__instance.version):
+            for j in range(self.__instance.version):
+                print (self.__instance.Grid[i][j], end='\t')
+            print ('')    
     
     def __getattr__(self, name):
         return getattr(self.__instance, name)
@@ -84,7 +79,7 @@ if __name__ == '__main__':
     print ('InputFilePath: {}'.format(inputPath))
     print ('OutputFilePath: {}'.format(outputPath))
 
-    s = State(3)
+    s = State(3, '3.txt')
     print('version: {}'.format(s.version))
     
     s.printGrid()
