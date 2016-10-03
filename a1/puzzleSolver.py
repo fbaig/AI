55f650c05dae8e377adc3d448a095df021caa2f2
Furqan Baig <furqan@Furqans-MacBook-Pro.local>
2016-10-03 17:13:44 -0400

    BFS + Priority Queue + Uniform Cost

--- a1/puzzleSolver.py
+++ a1/puzzleSolver.py
@@ -8,15 +8,25 @@ import sys
 BLANK = -9
 GOAL_STATE = None
 
-class Move:
-    ''' Essentially an Enum to represent move directions '''
-    LEFT, RIGHT, UP, DOWN = range(4)
+from enum import Enum
+Move = Enum('Move', 'LEFT RIGHT UP DOWN')
 
 class State(object):
     ''' State class to track each tile in the puzzle grid '''
 
-    def __init__(self, version, initFileName=None):
+    def __init__(self, version, initFileName=None, grid=None, parent=None, direction=None):
+        '''
+        Args:
+            version (int):      3 for 8 tile, 4 for 15 tile
+            initFileName (str): Input file to read initial state data from
+            grid (int[][]):     Initialize state from given grid state
+            parent (State):     For path backtracking in algorithms
+            direction (Move):   The move from parent to this state
+        '''
         self.version = version
+        # for backtracking
+        self.parent = parent
+        self.direction = direction
         # load state info from file
         if initFileName is not None:
             import csv
@@ -25,6 +35,9 @@ class State(object):
                 grid = list(reader)
             # convert all to numbers
             self.Grid = [[int(x) if x else int(BLANK) for x in inner] for inner in grid]
+        # create object from given state
+        elif(grid is not None):
+            self.Grid = grid
         # goal state
         else:
             self.Grid = [[(self.version*y)+x+1 for x in range(self.version)] for y in range(self.version)]
@@ -32,6 +45,11 @@ class State(object):
 
         # get and store blank index
         self.bi, self.bj = self.find(BLANK)
+
+        # the actual physical cost to reach this state from start
+        self.g_cost = 0
+        # heuristic cost to reach goal from this state
+        self.h_cost = 0
         
     def find(self, val):
         ''' 
@@ -51,7 +69,9 @@ class State(object):
         ''' 
         Move blank in given direction 
         Args:
-            direction (Move): Direction to move blank
+            direction (Move):    Direction to move blank
+        Return:
+            New object after move. Note, current state remains unchanged
         '''
 
         # Out of bound movement handling
@@ -59,7 +79,10 @@ class State(object):
             direction == Move.RIGHT and self.bj == self.version-1 or 
             direction == Move.UP  and self.bi == 0 or 
             direction == Move.DOWN and self.bi == self.version-1):
-            raise ValueError('Invalid move: Moving out of bound')
+            return None
+            #raise ValueError('Invalid move: Moving out of bound')
+
+        #self.direction = direction
         
         # Valid move, swap blank with value from desired direction
         if (direction == Move.LEFT):
@@ -73,40 +96,92 @@ class State(object):
 
         move_index_value = self.Grid[move_index_i][move_index_j]
 
+        # make a copy of
+        grid = [x[:] for x in self.Grid]
+        
         # swap values
-        self.Grid[move_index_i][move_index_j] = BLANK
-        self.Grid[self.bi][self.bj] = move_index_value
+        grid[move_index_i][move_index_j] = BLANK
+        grid[self.bi][self.bj] = move_index_value
         # update blank index
-        self.bi, self.bj = move_index_i, move_index_j
-    
-    def setTileValue(self, i, j, val):
-        # check for out of bound indices
-        if i < 0 or i > self.version or j < 0 or j > self.version:
-            print ('ERROR')
-        self.Grid[i][j] = val
+        #grid.bi, grid.bj = move_index_i, move_index_j
 
-    def getTileValue(self, i, j):
-        return self.Grid[i][j]
-
-    def printGrid(self):
-        print ('--------------------------------')
+        return State(self.version, grid=grid, parent=self, direction=direction)
+        
+    def __repr__(self):
+        ret = '\n------------------------\n'
         for i in range(self.version):
             for j in range(self.version):
                 # just for proper indentation/spacing
                 if self.Grid[i][j] > 0 and self.Grid[i][j] < 10:
-                    print (self.Grid[i][j], end='   |  ')
+                    ret += str(self.Grid[i][j]) + '   |  '
                 else:
-                    print (self.Grid[i][j], end='  |  ')
-            print ('')
-        print ('--------------------------------')
+                    ret += str(self.Grid[i][j]) + '  |  '
+            ret += '\n'
+        ret += '------------------------\n'
+        return ret
 
+    def __lt__(self, state2):
+        return (self.g_cost + self.h_cost) < (state2.g_cost + state2.h_cost)
+        
     def __eq__(self, state2):
         ''' Overriden equality operator to check states if they are equal based on their grid values '''
         return self.Grid == state2.Grid
-    
-    def __getattr__(self, name):
-        return getattr(self, name)
 
+############################################## Algo ##########################################
+
+class Algo():
+
+    def __init__(self, start):
+        self.start = start
+        self.start.g_cost = 0
+
+    def track_path(self, goal):
+        path = []
+        node = goal
+        # print path
+        while node.parent is not None:
+            path.append(node)
+            node = node.parent
+
+        path.reverse()
+        for s in path:
+            print (str(s.direction) + ', ')
+        
+    def apply_action(self, current, move):
+        new = current.move(move)
+        if new and new not in self.explored:
+            new.g_cost = current.g_cost + 1
+            if new == GOAL:
+                print ('GOAL REACHED ...' + str(new))
+                self.track_path(new)
+                return True
+            self.frontier.put(new)
+            
+    def bfs(self):
+        ''' Simple blind Breath First Search '''
+        import queue as Q
+
+        self.frontier = Q.PriorityQueue()
+        #self.frontier = Q.Queue()
+        self.frontier.put(self.start)
+    
+        self.explored = []
+        while not self.frontier.empty():
+            current = self.frontier.get()
+            if current == GOAL:
+                print ('GOAL REACHED ...')
+                return True
+            self.explored.append(current)
+            # generate all possible states by moving blank L, R, U, D
+            if self.apply_action(current, Move.LEFT): return True
+            elif self.apply_action(current, Move.RIGHT): return True
+            elif self.apply_action(current, Move.UP): return True
+            elif self.apply_action(current, Move.DOWN): return True
+        
+    def printFrontier(self):
+        while not self.frontier.empty():
+            print(self.frontier.get())
+        
 if __name__ == '__main__':
 
     if len(sys.argv) != 5:
@@ -117,7 +192,7 @@ if __name__ == '__main__':
             INPUT_FILE_PATH:\t self explanatory
             OUTPUT_FILE_PATH:\t self explanatory
         ''')
-    ########################################## CLI argument parsing ###################################
+    #################################### CLI argument parsing #############################
     algo = int(sys.argv[1])
     puzzle = int(sys.argv[2])
     inputPath = sys.argv[3]
@@ -140,21 +215,12 @@ if __name__ == '__main__':
     print ('InputFilePath: {}'.format(inputPath))
     print ('OutputFilePath: {}'.format(outputPath))
 
-    ###################################################################################################
+    #######################################################################################
     
     s = State(puzzle, '3.txt')
-    s.printGrid()
-
-    s.move(Move.LEFT)
-    s.move(Move.DOWN)
-    s.move(Move.RIGHT)
-    s.move(Move.DOWN)
-    
-    s.printGrid()
     
     global GOAL
     GOAL = State(puzzle)
 
-    if s == GOAL:
-        print('YAAAAAAYYY ....')
-    
+    a = Algo(s)
+    a.bfs()
