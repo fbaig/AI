442ec8308bf87f8a62e9fe03bacbab02767361cc
bmi-baig <fbaig@cs.stonybrook.edu>
2016-11-30 17:19:11 -0500

    baseline done with graph and table

--- a3/example.py
+++ a3/example.py
@@ -36,7 +36,8 @@ Hyperparameters:
 from sklearn.linear_model import LogisticRegression
 _reg_constant = 1
 _iter_count = 2
-LR = LogisticRegression(C=_reg_constant, max_iter=_iter_count)
+#LR = LogisticRegression(C=_reg_constant, max_iter=_iter_count)
+LR = LogisticRegression()
 
 '''
 SVM
@@ -47,7 +48,8 @@ Hyperparameters:
 from sklearn import svm
 _reg_constant = 2
 _kernel = 'linear' # rbf, linear, poly
-SVM = svm.SVC(C=_reg_constant, kernel=_kernel, degree=3)
+#SVM = svm.SVC(C=_reg_constant, kernel=_kernel, degree=3)
+SVM = svm.SVC()
 
 '''
 Random Forest
@@ -58,7 +60,8 @@ Hyperparameters:
 from sklearn.ensemble import RandomForestClassifier
 _trees_count = 10
 _features_count = 8
-RF = RandomForestClassifier(n_estimators=_trees_count, max_features=2)
+#RF = RandomForestClassifier(n_estimators=_trees_count, max_features=2)
+RF = RandomForestClassifier()
 
 
 
@@ -119,9 +122,9 @@ class Config:
         
         self.transformer = TfidfTransformer()
         if vectorizer == 'count':
-            self.vectorizer = CountVectorizer(lowercase=lowercase, stop_words=stopwords, ngram_range=(1, ngram))
+            self.vectorizer = CountVectorizer(lowercase=lowercase, stop_words=stopwords, ngram_range=(ngram, ngram))
         elif vectorizer == 'tfidf':
-            self.vectorizer = TfidfVectorizer(lowercase=lowercase, stop_words=stopwords, ngram_range=(1, ngram))
+            self.vectorizer = TfidfVectorizer(lowercase=lowercase, stop_words=stopwords, ngram_range=(ngram, ngram))
         else:
             self.vectorizer = None
 
@@ -135,7 +138,7 @@ class Config:
         elif classifier_str == 'RandomForestClassifier':
             classifier_str += '(# trees: %s, # features: %s)' % (str(self.classifier.n_estimators), str(self.classifier.max_features))
 
-        ret += 'classifier: %s\n' % classifier_str
+        ret += 'classifier: %s' % classifier_str
         return ret
 
 # configurations
@@ -151,47 +154,102 @@ BASELINE_SVM_BI = Config(classifier=SVM, ngram=2)
 BASELINE_RF_UNI = Config(classifier=RF)
 BASELINE_RF_BI = Config(classifier=RF, ngram=2)
 
-if __name__ == '__main__':
-
+def plot(datasize, f1values_nb, f1values_lr, f1values_svm, f1values_rf):
+    import matplotlib.pyplot as plt
+    #radius = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0]
+    #area = [3.14159, 12.56636, 28.27431, 50.26544, 78.53975, 113.09724]
+    plt.plot(datasize, f1values_nb, label='NB', linewidth=1.5, marker='s')
+    plt.plot(datasize, f1values_lr, label='LR', linestyle='--', marker='o', linewidth=1.5)
+    plt.plot(datasize, f1values_svm, label='SVM', linestyle=':', marker='+', linewidth=1.5)
+    plt.plot(datasize, f1values_rf, label='RF', linestyle='-.', marker='*', linewidth=1.5)
     
-    # configuration to use
-    conf = BASELINE_RF_BI
-    print (conf)
+    plt.xlabel('Data Size (# of documents)')
+    plt.ylabel('F1 Score')
+    plt.title('Classifier Performance with Unigram Representation')
+    plt.legend()
+    plt.show()
 
-    ############### Load Training Dataset #############
-    train_data_path = '/home/bmi-baig/Downloads/cse537-AI/AI/a3/selected_20NewsGroup/Training'
-    test_data_path = '/home/bmi-baig/Downloads/cse537-AI/AI/a3/selected_20NewsGroup/Test'
-    print ('Loading dataset from path: %s' % train_data_path)
+def classify_using(train_data_path, train_size, config, test_data_path):
 
     train_dataset = load_files(container_path=train_data_path, encoding='latin-1')
+    # maunally remove headers from training dataset
+    for i, doc in enumerate(train_dataset.data):
+        if i > train_size:
+            train_dataset.data[i] = ''
+            continue
+        preprocessed = doc.split('\n')
+        # Assuming 'Lines: <int>' is the last line in header
+        body_size = 0
+        for line in preprocessed:
+            if line.startswith('Lines'):
+                body_size = int(line.split(':')[1])+1
+            elif body_size > 0:
+                break
+            continue
 
-    print("%d documents" % len(train_dataset.data))
-    print("%d categories" % len(train_dataset.target_names))
-    print()
-    labels = train_dataset.target
-    
+        preprocessed = preprocessed[-body_size:]
+        train_dataset.data[i] = '\n'.join(preprocessed)
+
     classifier = Pipeline([('vectorizer', conf.vectorizer),
                            ('transformer', conf.transformer),
                            ('classifier', conf.classifier)])
-
+    
     _ = classifier.fit(train_dataset.data, train_dataset.target)
     ############## Test Data ##########################
     test_dataset = load_files(container_path=test_data_path, encoding='latin-1')
     # vectors_test = vectorizer.transform(test_dataset.data)
-
-    my_test_set = ['nfl is boring ...', 'christ is a prophet of God', 'the resident doctor was not on duty that day']
     ############## Predict ############################
-    print ('Predicting Test data')
-
-    predict1 = classifier.predict(my_test_set)
-    print (labels)
-    print (predict1)
-
+    #print ('Predicting Test data')
+    #my_test_set = ['nfl is boring ...', 'christ is a prophet of God', 'the resident doctor was not on duty that day']
+    #predict1 = classifier.predict(my_test_set)
+    #print (labels)
+    #print (predict1)
+    
     predict = classifier.predict(test_dataset.data)
+    
+    #import numpy as np
+    #print ('Accuracy: %f' % np.mean(predict == test_dataset.target))
+    return metrics.f1_score(test_dataset.target, predict, average='macro')
+        
+        
+if __name__ == '__main__':
 
-    import numpy as np
-    print ('Accuracy: %f' % np.mean(predict == test_dataset.target))
-    print ('F1-score: %f' % metrics.f1_score(test_dataset.target, predict, average='macro'))
+    baseline_configs_uni = [BASELINE_NB_UNI, BASELINE_LR_UNI, BASELINE_SVM_UNI, BASELINE_RF_UNI]
+    baseline_configs_bi = [BASELINE_NB_BI, BASELINE_LR_BI, BASELINE_SVM_BI, BASELINE_RF_BI]
+    
+
+    ############### Load Training Dataset #############
+    train_data_path = '/home/bmi-baig/Downloads/cse537-AI/AI/a3/selected_20NewsGroup/Training'
+    test_data_path = '/home/bmi-baig/Downloads/cse537-AI/AI/a3/selected_20NewsGroup/Test'
+    train_dataset = load_files(container_path=train_data_path, encoding='latin-1')
+
+    dataset_sizes = []
+    for i in range(100, len(train_dataset.data), 100):
+        dataset_sizes.append(i)
+
+    dataset_sizes.append(len(train_dataset.data))
+    print (dataset_sizes)
+    
+    #print (train_dataset.data[17])
+    #print("%d documents" % len(train_dataset.data))
+    #print("%d categories" % len(train_dataset.target_names))
+    #labels = train_dataset.target
+
+    classifier_f1_scores = []
+    for conf in baseline_configs_uni:
+        print ('Dataset path: %s' % test_data_path)
+        print (conf)
+        f1_scores = []
+        for train_size in dataset_sizes:
+            f1_score = classify_using(train_data_path, train_size, conf, test_data_path)
+            f1_scores.append(f1_score)
+            print ('F1-score: %f' % f1_score)
+            
+        classifier_f1_scores.append(f1_scores)
+        #break
+
+    plot(dataset_sizes, classifier_f1_scores[0], classifier_f1_scores[1], classifier_f1_scores[2], classifier_f1_scores[3])
+
 
 
 # # Display progress logs on stdout
